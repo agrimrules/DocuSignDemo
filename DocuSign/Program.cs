893e6267833982495117a6083b0e2c37fe0cf321
Agrim <agrimasthana@gmail.com>
2016-06-15 22:35:37 -0500

    Happy Path Achieved  :) new method and test added.

--- DocuSign/Program.cs
+++ DocuSign/Program.cs
@@ -9,15 +9,38 @@ namespace DocuSign
         private static string[] HOT = { null, "sandals", "sun visor", null, "T-shirt", null, "shorts", "leaving house", "Removing PJ's" };
         private static string[] COLD = { null, "boots", "hats", "socks", "shirt", "jacket", "pants", "leaving house", "Removing PJ's" };
         private static string[] INPUT = { "HOT", "8", "6", "4", "2", "1", "7" };
-        private static string[] ALL = { "1", "2","3","4","5","6","7","8"};
+        private static string[] ALL = { "1", "2","3","4","5","6","7","8","HOT","COLD"};
+
         static void Main(string[] args)
-        { 
-            //Console.WriteLine(isValid);
-            //PintOutput(INPUT);
+        {
+            Rule1(INPUT);
+            int r2 = Rule2(INPUT);
+            int r3 = Rule5and6(INPUT);
+            int r4 = Rule7(INPUT);
+            int r5 = Rule8(INPUT);
+            int r6 = Rule9(INPUT);
+            int[] idx = { r2,r3,r4,r5,r6};
+            if (r2== -1 && r3 == -1 && r4 == -1 && r5 == -1 && r6 == -1)
+            {
+            PrintOutput(INPUT, INPUT.Length);
+            }
+            else
+            {
+                int failat = idx.Where(i => i > 0).Min();
+                PrintOutput(INPUT, failat);
+                Console.WriteLine("Fail");
+            }
+            Console.ReadLine(); //Making the program wait for return to be pressed 
         }
 
+        /* Basic idea is to return the index of the failing variable and print only till that.
+         * All of the rules below will return a flag of -1 if the Input array is valid.
+         *
+         */
+        
+
         //If the first step is not Removing PJ's we print failure.
-        private void Rule1(string [] arr)
+        public static void Rule1(string [] arr)
         {
             if(arr[1] != "8")
             {
@@ -26,7 +49,7 @@ namespace DocuSign
         }
 
         //If no instruction is repeating return -1 or else return index of repeating element.
-        public int Rule2(string[] arr)
+        public static int Rule2(string[] arr)
         {
             List<string> val = new List<string>();
             foreach (string s in arr)
@@ -41,54 +64,51 @@ namespace DocuSign
             return -1;
         }
 
-        public int Rule5and6 (string[] arr)
+        //If it is hot we should not wear socks or a jacket. Therefore find and report their indices.
+        public static int Rule3and4(string[] arr)
+        {
+            if(arr[0] == "HOT" && arr.Contains("3") | arr.Contains("5"))
+            {
+                int hotsock = Array.IndexOf(arr, "3");
+                int hotjack = Array.IndexOf(arr, "5");
+                int[] ix = { hotsock, hotjack };
+                return ix.Where(i => i > 0).Min();
+            }
+            return -1;
+        }
+
+        //We calculate the index of when the pants,socks and shoes are being put on.
+        //and then check if they are ordered correctly returning the index of the faulty instruction
+        public static int Rule5and6 (string[] arr)
         {
             int pantIndex = Array.FindIndex(arr, row => row.Contains("6"));
             int socksIndex = Array.FindIndex(arr, row => row.Contains("3"));
             int shoeIndex = Array.FindIndex(arr, row => row.Contains("1"));
             if(pantIndex > shoeIndex || socksIndex > shoeIndex)
             {
-                if(pantIndex < socksIndex && socksIndex != -1)
-                {
-                    return pantIndex;
-                }
-                else if(socksIndex < pantIndex && socksIndex != -1)
-                {
-                return socksIndex;
-                }
-                else if(socksIndex == -1)
-                {
-                    return pantIndex;
-                }
-                return socksIndex;
+                int[] r56 = { pantIndex, socksIndex };
+                return r56.Where(i => i > 0).Min();
             }
             return -1;
         }
 
-        public int Rule7(string[] arr)
+        //We calculate the index of when the shirt,headwear and Jacket are being put on.
+        //Also allow for the Jacket to have an index of -1 incase the Input is cold
+        //and then check if they are ordered correctly returning the index of the faulty instruction
+        public static int Rule7(string[] arr)
         {
             int shirtIndex = Array.FindIndex(arr, row => row.Contains("4"));
             int headIndex = Array.FindIndex(arr, row => row.Contains("2"));
             int JackIndex = Array.FindIndex(arr, row => row.Contains("5"));
-            if (shirtIndex > headIndex || shirtIndex > JackIndex) {
-                if (headIndex > JackIndex && JackIndex != -1)
-                {
-                    return JackIndex;
-                }
-                else if (JackIndex != -1 && shirtIndex > headIndex)
-                {
-                    return headIndex;
-                }
-                else if (JackIndex == -1)
-                {
-                    return headIndex;
-                }
-                return JackIndex;
+            if (headIndex > shirtIndex || JackIndex > shirtIndex) {
+                int[] r7 = { JackIndex, headIndex };
+                return r7.Where(i => i > 0).Min();
             }
             return -1;
         }
 
-        public int Rule8(string[] arr)
+        //Ensure that Leaving home is the last instruction in the input dataset
+        public static int Rule8(string[] arr)
         {
             int leavingIndex = Array.FindIndex(arr, row => row.Contains("7"));
             if (leavingIndex < arr.Length -1)
@@ -98,9 +118,10 @@ namespace DocuSign
             return -1;
         }
 
-        public int Rule9(string[] arr)
+        //If we come across any input string that isn't in the predefined list of instructions return its index.
+        public static int Rule9(string[] arr)
         {
-            for(int i = 1;i<arr.Length;i++)
+            for(int i = 0;i<arr.Length;i++)
             {
             if(!ALL.Contains(arr[i]))
             {
@@ -108,16 +129,10 @@ namespace DocuSign
             }
             }
             return -1;
-
-            //var res = arr.Where(p => !HOT.Any() | !COLD.Any());
-            //if (res != null)
-            //{
-            //return Array.IndexOf(arr,res);
-            //}
-            //return -1;
         }
 
-        private static void PintOutput(string[] a, int Failat)
+        //Print the output till we reach the failat index.
+        private static void PrintOutput(string[] a, int Failat)
         {
             switch (a[0])
             {
--- DocuSignTests/ProgramTests.cs
+++ DocuSignTests/ProgramTests.cs
@@ -11,15 +11,29 @@ namespace DocuSign.Tests
     [TestClass()]
     public class ProgramTests
     {
-        public Program prgm = new Program();
+        /*
+         * In all of these unit tests an Input stream is prvodided for success and failure
+         * We must inspect each validation function For both success and failure
+         */
+
         [TestMethod()]
         public void Rule2Test()
         {
             string[] INPUTSuccess = { "HOT", "8", "6", "4", "2", "1", "7" };
             string[] INPUTFail = { "HOT", "8", "6", "6", "2", "1", "7" };
-            //var prgm = new Program();
-            int resSuccess = prgm.Rule2(INPUTSuccess);
-            int resFail = prgm.Rule2(INPUTFail);
+            int resSuccess = Program.Rule2(INPUTSuccess);
+            int resFail = Program.Rule2(INPUTFail);
+            Assert.AreEqual(resSuccess, -1);
+            Assert.AreEqual(resFail, 2);
+        }
+
+        [TestMethod()]
+        public void Rule3and4Test()
+        {
+            string[] INPUTSuccess = { "HOT", "8", "6", "4", "2", "1", "7" };
+            string[] INPUTFail = { "HOT", "8", "3", "6", "2", "1", "7" };
+            int resSuccess = Program.Rule3and4(INPUTSuccess);
+            int resFail = Program.Rule3and4(INPUTFail);
             Assert.AreEqual(resSuccess, -1);
             Assert.AreEqual(resFail, 2);
         }
@@ -30,10 +44,9 @@ namespace DocuSign.Tests
             string[] INPUTSuccess = { "HOT", "8", "6", "4", "2", "1", "7" };
             string[] INPUTFailPants = { "HOT", "8", "1", "4", "2", "6", "7" };
             string[] INPUTFailSocks = { "COLD", "8", "1", "4", "3", "6", "7" };
-            //var prgm = new Program();
-            int resSuccess = prgm.Rule5and6(INPUTSuccess);
-            int PantFail = prgm.Rule5and6(INPUTFailPants);
-            int SocksFail = prgm.Rule5and6(INPUTFailSocks);
+            int resSuccess = Program.Rule5and6(INPUTSuccess);
+            int PantFail = Program.Rule5and6(INPUTFailPants);
+            int SocksFail = Program.Rule5and6(INPUTFailSocks);
             Assert.AreEqual(resSuccess, -1);
             Assert.AreEqual(PantFail, 5);
             Assert.AreEqual(SocksFail, 4);
@@ -42,13 +55,15 @@ namespace DocuSign.Tests
         [TestMethod()]
         public void Rule7Test()
         {
-            string[] INPUTFail = { "HOT", "8", "2", "4", "6", "1", "7" };
-            string[] INPUTSuccess = { "HOT", "8", "1", "4", "2", "6", "7" };
-            //var prgm = new Program();
-            int resSuccess = prgm.Rule7(INPUTSuccess);
-            int resFail = prgm.Rule7(INPUTFail);
-            Assert.AreEqual(resSuccess, 4);
-            Assert.AreEqual(resFail, 2);
+            string[] INPUTSuccess = { "HOT", "8", "1", "2","6","4","7" };
+            string[] INPUTFailhead = { "HOT", "8", "4", "6", "1", "2", "7" };
+            string[] INPUTFailJacket = { "COLD", "8", "6", "3", "5", "4", "2", "1", "7" };
+            int resSuccess = Program.Rule7(INPUTSuccess);
+            int resFailhead = Program.Rule7(INPUTFailhead);
+            int resFailJack = Program.Rule7(INPUTFailJacket);
+            Assert.AreEqual(resSuccess, -1);
+            Assert.AreEqual(resFailhead, 5);
+            Assert.AreEqual(resFailJack, 4);
         }
 
         [TestMethod()]
@@ -56,8 +71,8 @@ namespace DocuSign.Tests
         {
             string[] INPUTSuccess = { "HOT", "8", "6", "4", "2", "1", "7" };
             string[] INPUTFail = { "HOT", "8", "6", "6", "2", "7", "1" };
-            int resSuccess = prgm.Rule8(INPUTSuccess);
-            int resFail = prgm.Rule8(INPUTFail);
+            int resSuccess = Program.Rule8(INPUTSuccess);
+            int resFail = Program.Rule8(INPUTFail);
             Assert.AreEqual(resSuccess, -1);
             Assert.AreEqual(resFail, 5);
         }
@@ -67,10 +82,11 @@ namespace DocuSign.Tests
         {
             string[] INPUTSuccess = { "HOT", "8", "6", "4", "2", "1", "7" };
             string[] INPUTFail = { "HOT", "8", "6", "6", "2", "bar", "1" };
-            int resSuccess = prgm.Rule9(INPUTSuccess);
-            int resFail = prgm.Rule9(INPUTFail);
+            int resSuccess = Program.Rule9(INPUTSuccess);
+            int resFail = Program.Rule9(INPUTFail);
             Assert.AreEqual(resSuccess, -1);
             Assert.AreEqual(resFail, 5);
         }
+
     }
 }
